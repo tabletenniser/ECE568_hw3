a675b22f4af53733690eefc32dd275494a2e6782
tabletenniser <zexuan.wang@mail.utoronto.ca>
2016-03-17 22:03:08 -0400

    finish part1

--- generateQRcode.c
+++ generateQRcode.c
@@ -13,19 +13,54 @@ main(int argc, char * argv[])
 		return(-1);
 	}
 
-	char *	issuer = argv[1];
-	char *	accountName = argv[2];
-	char *	secret_hex = argv[3];
+	const char* issuer = argv[1];
+	const char* accountName = argv[2];
+	char* pre_secret_hex = argv[3];
+    const char* issuerEncoded;
+    const char* accountNameEncoded;
+    char secret_hex[21];
+	uint8_t secret[10];
+	char otpauth[200];
+    char buf[100];
 
-	assert (strlen(secret_hex) <= 20);
+	assert (strlen(pre_secret_hex) <= 20 && strlen(pre_secret_hex) > 0);
+
+    // Append zeros to the beginning if less than 20 digits
+    int idx = strlen(pre_secret_hex) - 1;
+    int cp_idx = 19;
+    secret_hex[20] = '\0';
+    for(; idx >=0; idx--, cp_idx--){
+        secret_hex[cp_idx] = pre_secret_hex[idx];
+    }
+    while(cp_idx >= 0){
+        secret_hex[cp_idx] = '0';
+        cp_idx--;
+    }
 
 	printf("\nIssuer: %s\nAccount Name: %s\nSecret (Hex): %s\n\n",
 		issuer, accountName, secret_hex);
 
+    // Convert secret to uint8_t*
+    int i;
+    for (i = 0; i < strlen(secret_hex); i += 2){
+        secret[i/2] = (uint8_t) ((secret_hex[i] - '0') * 16 + (secret_hex[i+1] - '0'));
+    }
+
 	// Create an otpauth:// URI and display a QR code that's compatible
 	// with Google Authenticator
+    // otpauth://hotp/ACCOUNTNAME?issuer=ISSUER&secret=SECRET&counter=1
+    // otpauth://totp/ACCOUNTNAME?issuer=ISSUER&secret=SECRET&period=30
+    accountNameEncoded = urlEncode(accountName);
+    issuerEncoded = urlEncode(issuer);
+    int count = base32_encode(secret, strlen(secret_hex)/2, (uint8_t *) &buf[0], 100);
 
-	displayQRcode("otpauth://testing");
+    // Display the HOTP
+    sprintf(otpauth, "otpauth://hotp/%s?issuer=%s&secret=%s&counter=1", accountNameEncoded, issuerEncoded, buf);
+	displayQRcode(otpauth);
+
+    // Display the TOTP
+    sprintf(otpauth, "otpauth://totp/%s?issuer=%s&secret=%s&period=30", accountNameEncoded, issuerEncoded, buf);
+	displayQRcode(otpauth);
 
 	return (0);
 }
