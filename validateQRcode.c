6728daf0bf7e0c83e88229a328e8b4d62b52c960
tabletenniser <zexuan.wang@mail.utoronto.ca>
2016-03-20 19:09:29 -0400

    finally finishedmake && ./validateQRcode 12345678901234567890 803282 758738

--- validateQRcode.c
+++ validateQRcode.c
@@ -11,31 +11,50 @@
 void HMAC(char *, char *, uint8_t *);
 void hmac_fcn(unsigned char*, int, unsigned char *, int, uint8_t *);
 
+void longToBytes(long num, unsigned char arr[4])
+{
+    arr[0]= (int)((num >> 24) & 0xFF);
+    arr[1]= (int)((num >> 16) & 0xFF);
+    arr[2]= (int)((num >> 8) & 0xFF);
+    arr[3]= (int)( num & 0xFF);
+}
+
+void longToBytes2(long long_num, unsigned char arr[8]){
+    int i;
+    for (i=7; i>=0; i--){
+        arr[i] = long_num & 0xff;
+        long_num >>= 8;
+    }
+}
+
+uint8_t hexFromChar(char c)
+{
+	if(c >= '0' && c <= '9') return c - '0';
+	if(c >= 'a' && c <= 'f') return c - 'a' + 10;
+	if(c >= 'A' && c <= 'F') return c - 'A' + 10;
+	return 255;
+}
+
+
+
 static int
 validateHOTP(char * secret_hex, char * HOTP_string)
 {
     uint8_t hmac[SHA1_DIGEST_LENGTH];
     // 1) COMPUTE HMAC CODE VALUE
-    /* HMAC(secret_hex, "1", hmac); */
-    hmac_fcn("1", 1, secret_hex, 20, hmac);
-    printf("Key is %s, msg is %s, SHA1=%s\n", secret_hex, "1", hmac);
-
-    printf("\nFINAL HMAC:\n");
-    int i = 0;
-    for(; i < SHA1_DIGEST_LENGTH; i++){
-        printf("%02x", hmac[i]);
-    }
-    printf("\n");
+    long counter = 1;
+    char counterChar[8];
+    longToBytes2(counter, counterChar);
+    hmac_fcn(counterChar, 8, secret_hex, 20, hmac);
+    /* HMAC(secret_hex, counterChar, hmac); */
 
     // 2) TRUNCATE AND COMPUTE HOTP VALUE
-    int offset = hmac[19] & 0xf;
-    printf("Offset is: %d\n", offset);
-    int bin_code = (hmac[offset] & 0x7f) << 24
+    uint8_t offset = hmac[SHA1_DIGEST_LENGTH-1] & 0xf;
+    long bin_code = (hmac[offset] & 0x7f) << 24
         | (hmac[offset+1] & 0xff) << 16
         | (hmac[offset+2] & 0xff) << 8
         | (hmac[offset+3] & 0xff);
     bin_code = bin_code % 1000000;
-    printf("bin_code is: %d\n", bin_code);
 
     if (atoi(HOTP_string) == bin_code){
         return 1;
@@ -52,7 +71,6 @@ void hmac_fcn(text, text_len, key, key_len, final_sha)
     int key_len; /* length of authentication key */
     uint8_t* final_sha; /* length of authentication key */
 {
-    printf("text is %s, text_len is %d, key is %s, key_len is %d\n", text, text_len, key, key_len);
     SHA1_INFO context;
     unsigned char k_ipad[65]; /* inner padding -
                                * key XORd with ipad
@@ -62,20 +80,17 @@ void hmac_fcn(text, text_len, key, key_len, final_sha)
                                *  */
     unsigned char tk[16];
     int i;
-    /* if key is longer than 64 bytes reset it to key=MD5(key) */
-    /* if (key_len > 64) { */
-    /*     MD5_CTX tctx; */
-    /*     MD5Init(&tctx); */
-    /*     MD5Update(&tctx, key, key_len); */
-    /*     MD5Final(tk, &tctx); */
-    /*     key = tk; */
-    /*     key_len = 16; */
-    /* } */
-    /* start out by storing key in pads */
+
+	uint8_t key_hex_int[10];
+	for (i = 0; i < 10; i++) {
+		key_hex_int[i] = ((0xf & (hexFromChar(key[i*2]))) << 4)| (0xf & (hexFromChar(key[i*2+1])));
+		/* key_hex_int[i] = ((0xf & (key[i * 2] - '0')) << 4)| (0xf & (key[i * 2 + 1] - '0')); */
+    }
+
     bzero( k_ipad, sizeof k_ipad);
     bzero( k_opad, sizeof k_opad);
-    bcopy( key, k_ipad, key_len);
-    bcopy( key, k_opad, key_len);
+    bcopy( key_hex_int, k_ipad, 10);
+    bcopy( key_hex_int, k_opad, 10);
 
     /* XOR key with ipad and opad values */
     for (i=0; i< KEY_LENGTH; i++) {
@@ -83,20 +98,8 @@ void hmac_fcn(text, text_len, key, key_len, final_sha)
         k_opad[i] ^= 0x5c;
     }
 
-    printf("\nBEFORE XOR - ipad: ");
-    i = 0;
-    for(; i < KEY_LENGTH; i++){
-        printf("%02x", k_ipad[i]);
-    }
-    printf("\nBEFORE XOR - opad: ");
-    i = 0;
-    for(; i < KEY_LENGTH; i++){
-        printf("%02x", k_opad[i]);
-    }
-    printf("\n");
-
     /*
-     *  * perform inner MD5
+     *  * perform inner SHA1
      *   */
     /* uint8_t *digest = (uint8_t) malloc(SHA1_DIGEST_LENGTH * sizeof(uint8_t)); */
     uint8_t digest[SHA1_DIGEST_LENGTH];
@@ -104,13 +107,6 @@ void hmac_fcn(text, text_len, key, key_len, final_sha)
     sha1_update(&context, k_ipad, KEY_LENGTH); /* start with inner pad */
     sha1_update(&context, text, text_len); /* then text of datagram */
     sha1_final(&context, digest); /* finish up 1st pass */
-
-    printf("\nSHA1:\n");
-    i = 0;
-    for(; i < SHA1_DIGEST_LENGTH; i++){
-        printf("%02x", digest[i]);
-    }
-    printf("\n");
 
     /*
      *  * perform outer MD5
@@ -120,21 +116,12 @@ void hmac_fcn(text, text_len, key, key_len, final_sha)
     sha1_update(&context, digest, SHA1_DIGEST_LENGTH); /* then results of 1st * hash */
     sha1_final(&context, final_sha); /* finish up 2nd pass */
 
-    printf("\nFINAL SHA:\n");
-    i = 0;
-    for(; i < SHA1_DIGEST_LENGTH; i++){
-        printf("%02x", final_sha[i]);
-    }
-    printf("\n");
-
     return;
 }
 
 
 void HMAC(char *key, char *msg, uint8_t* sha_final){
-    printf("msg: %s\n", msg);
     int key_len = strlen(key);
-    printf("key: %s -l1: %d -l2: %d\n", key, key_len, strlen(key));
     assert(key_len <= 64);
 
     // STEP #1: Set up ipad[] and opad[] to be '5c5c5c...' and '363636...'
@@ -145,35 +132,11 @@ void HMAC(char *key, char *msg, uint8_t* sha_final){
     memset(&ipad[0], '\x36', KEY_LENGTH);
     memset(&opad[0], '\x5c', KEY_LENGTH);
 
-    printf("\nBEFORE XOR - ipad: ");
     int i = 0;
-    for(; i < KEY_LENGTH; i++){
-        printf("%02x", ipad[i]);
-    }
-    printf("\nBEFORE XOR - opad: ");
-    i = 0;
-    for(; i < KEY_LENGTH; i++){
-        printf("%02x", opad[i]);
-    }
-    printf("\n");
-
-    i = 0;
     for(; i < key_len; i++){
         ipad[i] ^= (uint8_t) (key[i]);
         opad[i] ^= (uint8_t) (key[i]);
     }
-
-    printf("\nAFTER XOR - ipad: ");
-    i = 0;
-    for(; i < KEY_LENGTH; i++){
-        printf("%02x", ipad[i]);
-    }
-    printf("\nAFTER XOR - opad: ");
-    i = 0;
-    for(; i < KEY_LENGTH; i++){
-        printf("%02x", opad[i]);
-    }
-    printf("\n");
 
     // prepare for SHA1
     int key_msg_len = strlen(msg) + KEY_LENGTH;
@@ -182,21 +145,7 @@ void HMAC(char *key, char *msg, uint8_t* sha_final){
 
     // construct SHA1 argument
     memcpy(tmp, ipad, KEY_LENGTH);
-    printf("\nBEFORE STRCAT() - tmp: ");
-    i = 0;
-    for(; i < key_msg_len; i++){
-        printf("%02x", tmp[i]);
-    }
-    printf("\n");
-
     memcpy(&tmp[KEY_LENGTH], msg, strlen(msg)+1);
-
-    printf("\nAFTER STRCAT() - tmp: ");
-    i = 0;
-    for(; i < key_msg_len; i++){
-        printf("%02x", tmp[i]);
-    }
-    printf("\n");
 
     // 1st sha
     SHA1_INFO ctx;
@@ -205,45 +154,17 @@ void HMAC(char *key, char *msg, uint8_t* sha_final){
     sha1_update(&ctx, tmp, key_msg_len);
     sha1_final(&ctx, sha);
 
-    printf("\nSHA1:\n");
-    i = 0;
-    for(; i < SHA1_DIGEST_LENGTH; i++){
-        printf("%02x", sha[i]);
-    }
-    printf("\n");
-
     // 2nd sha
     free(tmp);
     tmp = (uint8_t *) calloc(KEY_LENGTH + SHA1_DIGEST_LENGTH + 1, sizeof(uint8_t));
     memcpy(tmp, opad, KEY_LENGTH);
 
-    printf("\npre-sha2: ");
-    i = 0;
-    for(; i < key_msg_len; i++){
-        printf("%02x", tmp[i]);
-    }
-    printf("\n");
-
     memcpy(&tmp[KEY_LENGTH], sha, SHA1_DIGEST_LENGTH+1);
-
-    printf("\n???pre-sha2: %d\n", KEY_LENGTH+SHA1_DIGEST_LENGTH+1);
-    i = 0;
-    for(; i < KEY_LENGTH+SHA1_DIGEST_LENGTH+1; i++){
-        printf("%02x", tmp[i]);
-    }
-    printf("\n");
 
     SHA1_INFO ctx_again;
     sha1_init(&ctx_again);
     sha1_update(&ctx_again, tmp, KEY_LENGTH+SHA1_DIGEST_LENGTH);
     sha1_final(&ctx_again, sha_final);
-
-    printf("\nFINAL SHA:\n");
-    i = 0;
-    for(; i < SHA1_DIGEST_LENGTH; i++){
-        printf("%02x", sha_final[i]);
-    }
-    printf("\n");
 
     free(tmp);
     return;
@@ -256,32 +177,21 @@ validateTOTP(char * secret_hex, char * TOTP_string)
     // 1) COMPUTE TIME (msg) FOR HMAC
     time_t cur_time = time(NULL);
     long T = (cur_time- 0) / 30;
-    printf("T is: %d\n", T);
-    char T_str[64];
-    sprintf (T_str, "%lX", T);
-    printf("T_str is: %s\n", T_str);
+
+    char TChar[8];
+    longToBytes2(T, TChar);
 
     // 2) HMAC COMPUTATION WITH secret_hex and time_val
     /* HMAC(secret_hex, "1", hmac); */
-    hmac_fcn(T_str, strlen(T_str), secret_hex, 20, hmac);
-    printf("Key is %s, msg is %s, SHA1=%s\n", secret_hex, "1", hmac);
-
-    printf("\nFINAL HMAC:\n");
-    int i = 0;
-    for(; i < SHA1_DIGEST_LENGTH; i++){
-        printf("%02x", hmac[i]);
-    }
-    printf("\n");
+    hmac_fcn(TChar, 8, secret_hex, 20, hmac);
 
     // 2) TRUNCATE AND COMPUTE HOTP VALUE
     int offset = hmac[19] & 0xf;
-    printf("Offset is: %d\n", offset);
     int bin_code = (hmac[offset] & 0x7f) << 24
         | (hmac[offset+1] & 0xff) << 16
         | (hmac[offset+2] & 0xff) << 8
         | (hmac[offset+3] & 0xff);
     bin_code = bin_code % 1000000;
-    printf("bin_code is: %d\n", bin_code);
 
     if (atoi(TOTP_string) == bin_code){
         return 1;
